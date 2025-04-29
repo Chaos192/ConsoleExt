472a2b173aee5e00a92c082b6d4d4019570907ac
ch0nker <chonkers.git@gmail.com>
2025-04-29 02:27:57 -0700

    [rem]: delete to move header files

--- src/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include <Windows.h>
-
-std::uint8_t* PatternScan(void* module, const char* signature);
-std::uint8_t* PatternScan(void* module, const char* signature, uintptr_t offset);
